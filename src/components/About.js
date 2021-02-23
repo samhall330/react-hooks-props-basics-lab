66a2a0091864d6be7f60a6f5825152f3cfb525f0
Sam Hall <mann.samantha11@gmail.com>
2021-02-22 23:22:49 -0500

    commit tree 78a030b622db1b28bc3a66c1190fb86e6f6547e6

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About({user}) {
+  const {bio, links} = user
+  console.log(links)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links name="Github" link={links.github}/>
+      <Links name ="LinkedIn" link={links.linkedin}/>
     </div>
   );
 }
